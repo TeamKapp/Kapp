09c21393e64e3eb91d4301a257d25ca230c197a8
육진혁 <jdd04026@gmail.com>
2015-02-11 14:50:55 +0900

    대량삭

--- app/src/main/java/project/kapp/MainActivity.java
+++ app/src/main/java/project/kapp/MainActivity.java
@@ -1,18 +1,20 @@
 package project.kapp;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
-import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.a_main);
+
+        ActionBar actionBar = getActionBar();
 
         ViewPager pager = (ViewPager) findViewById(R.id.pager);
         pager.setAdapter(new MainSlideAdapter(getSupportFragmentManager()));
@@ -27,23 +29,19 @@ public class MainActivity extends ActionBarActivity{
 
         @Override
         public int getCount() {
-            return 6;
+            return 4;
         }
 
         public Fragment getItem(int position) {
             switch (position) {
                 case 0:
-                    return HomeFragment.newInstance(position);
-                case 1:
                     return RiceFragment.newInstance(position);
-                case 2:
+                case 1:
                     return TimetableFragment.newInstance(position);
-                case 3:
+                case 2:
                     return DdayFragment.newInstance(position);
-                case 4:
+                case 3:
                     return WordFragment.newInstance(position);
-                case 5:
-                    return MoreFragment.newInstance(position);
                 default:
                     return null;
             }
